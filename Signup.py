26ba3c7af746a9a4baa56c7cc8b8a8c9d9009d28
Elliot <Elliotduru600@gmail.com>
2022-02-01 23:09:35 +0100

    initial commit

--- Signup.py
+++ Signup.py
@@ -1,3 +1,8 @@
 Firstname = "Chidubem"
 Middlename = "Elliot"
 Surname = "Duruemeruo"
+Email = "Elliotduru600@gmail.com"
+D.O.B = "14th June 2003"
+PhoneNumber = "07063935013"
+PostalCode = "300271"
+
