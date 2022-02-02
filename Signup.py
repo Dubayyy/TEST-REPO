1657e8d93a84f8f33f5af97fa2eca27da7f8b8f1
Elliot <Elliotduru600@gmail.com>
2022-02-02 22:43:20 +0100

    initial commit

--- Signup.py
+++ Signup.py
@@ -2,7 +2,11 @@ Firstname = "Chidubem"
 Middlename = "Elliot"
 Surname = "Duruemeruo"
 Email = "Elliotduru600@gmail.com"
-D.O.B = "14th June 2003"
+DOB = "14th June 2003"
 PhoneNumber = "07063935013"
 PostalCode = "300271"
 
+if Firstname == "chidubem"
+  print ("successful")
+else 
+  print ("wrong firstname")  
