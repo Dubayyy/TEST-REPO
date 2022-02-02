7e681d370149ac5da0c6b790cd63563b0cf07306
Elliot <Elliotduru600@gmail.com>
2022-02-02 23:52:15 +0100

    initial commit

--- Signup.py
+++ Signup.py
@@ -6,7 +6,37 @@ DOB = "14th June 2003"
 PhoneNumber = "07063935013"
 PostalCode = "300271"
 
-if Firstname == "chidubem"
+if Firstname == "Chidubem":
   print ("successful")
-else 
-  print ("wrong firstname")  
+else :
+  print ("wrong firstname")
+
+if Middlename == "Elliot":
+  print ("successful")
+else :
+    print ("wrong Middleame")
+
+if Surname == "Duruemeruo":
+    print ("successful")
+else:
+    print ("incorrect Surname")
+
+if Email == "Elliotduru600@gmail.com":
+  print ("Successful")
+else:
+    print ("incorrect email")
+
+if DOB == "14th June 2007":
+    print ("Successful")
+else:
+    print ("Incorrect DOB")
+
+if PhoneNumber == "07063935013":
+    print ("Successful")
+else:
+    print ("Incorrect PhoneNumber")
+
+if PostalCode == "300271":
+    print ("Successful")
+else:
+    print ("Incorrect PostalCode")
